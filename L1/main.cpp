0cab697cf22910c3364dc84b9bf76246183b18d2
shuraaok <aleksandrakutepovaa@gmail.com>
2025-09-20 13:01:37 +0200

    add

---
+++ L1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int c;
+	c = sqrt(sqr(a) + sqr(b));
+	cout << c << endl;
+	int d;
+	d = a + b + c;
+	cout << d endl;
+}
